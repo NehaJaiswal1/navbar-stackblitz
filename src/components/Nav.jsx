7741d7cef14b97d46083e047df4d5d39d97b76e0
NehaJaiswal1 <101494440+NehaJaiswal1@users.noreply.github.com>
2024-02-21 21:53:29 +0530

    Initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,20 @@
+
+import React,{useState} from 'react'
+import './Nav.css'
+
+function Navbar () {
+return(
+ <>
+  <nav>
+    <ul>
+      <li><a href='#'>Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+ </>
+)
+}
+
+export default Navbar
